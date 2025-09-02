d732d8254c9d10d4d76a70d8a7c9689383de2dcf
niclbody13 <91341366+niclbody13@users.noreply.github.com>
2025-09-02 01:35:07 -0700

    add navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Navbar from "./components/Navbar";
 import "./globals.css";
 
 import { config } from '@fortawesome/fontawesome-svg-core'
@@ -31,6 +32,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navbar />
         {children}
       </body>
     </html>
